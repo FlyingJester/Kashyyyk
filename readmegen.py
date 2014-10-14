5ae97c6abfe4a4a8e1baedee45e36f438ba148f3
FlyingJester <foolkingcrown@gmail.com>
2014-10-13 19:48:04 -0800

    Improved CSS generator

--- readmegen.py
+++ readmegen.py
@@ -2,35 +2,45 @@ import os
 import sys
 import subprocess
 
+gradient_types =[
+ "-webkit-linear-gradient(left, ",
+ "-o-linear-gradient(     right,",
+ "-moz-linear-gradient(   right,",
+ "linear-gradient(     to right,"]
+
+gradient = "black 0px, #222233  calc(50% - ( 928px / 2 ) ), #222233  calc( 50% + ( 928px / 2 ) ), black 100%);"
+
 css = open("kashyyyk.css", "w")
 css.write("body {"
 "align: center;"
 "margin: 0px auto;"
 "width: 50%;"
 "color: #888899;"
-"font-family: \"DejaVu sans\";"
-"background: -webkit-linear-gradient(left,  black 0px, #222233  calc(50% - ( 928px / 2 ) ), #222233  calc( 50% + ( 928px / 2 ) ), black 100%);"
-"background: -o-linear-gradient(     right, black 0px, #222233  calc(50% - ( 928px / 2 ) ), #222233  calc( 50% + ( 928px / 2 ) ), black 100%);"
-"background: -moz-linear-gradient(   right, black 0px, #222233  calc(50% - ( 928px / 2 ) ), #222233  calc( 50% + ( 928px / 2 ) ), black 100%);"
-"background: linear-gradient(     to right, black 0px, #222233  calc(50% - ( 928px / 2 ) ), #222233  calc( 50% + ( 928px / 2 ) ), black 100%);"
-"}"
-"\n"
-"h1 {"
-"box-shadow: 10px 5px 5px #555577;"
-"color: black;"
-"padding-left: 1em;"
-"padding-right: 1em;"
-"padding-top: 0.5em;"
-"padding-bottom: 0.25em;"
-"border-color: #666666;"
-"background: #AAAAAA;"
-"border-radius:8px;"
-"border-color: #220202;"
-"}"
-"\n"
-"h2 {"
-"box-shadow: 5px 2px 2px #555577;"
-"}")
+"font-family: \"DejaVu sans\";")
+
+for i in gradient_types:
+  css.write("background: ")
+  css.write(i)
+  css.write(gradient)
+  css.write("\n")
+
+css.write("}\n")
+css.write("h1 {"
+    "box-shadow: 10px 5px 5px #555577;"
+    "color: black;"
+    "padding-left: 1em;"
+    "padding-right: 1em;"
+    "padding-top: 0.5em;"
+    "padding-bottom: 0.25em;"
+    "border-color: #666666;"
+    "background: #AAAAAA;"
+    "border-radius:8px;"
+    "border-color: #220202;"
+    "}"
+    "\n"
+    "h2 {"
+    "box-shadow: 5px 2px 2px #555577;"
+    "}")
 
 css.close()
 
