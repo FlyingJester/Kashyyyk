2cd8465a0033eb413188d3804fabddcfc271e05c
FlyingJester <foolkingcrown@gmail.com>
2014-10-18 03:09:12 -0800

    Added static icon library.

---
+++ extra/icons/format.h
@@ -0,0 +1,7 @@
+#pragma once
+
+struct Kashyyyk_BinImage {
+  const unsigned short width;
+  const unsigned short height;
+  char * pixel_data;
+};
---
+++ extra/icons/index.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "format.h"
+
+extern const struct Kashyyyk_BinImage kashyyyk_new_window_image;
